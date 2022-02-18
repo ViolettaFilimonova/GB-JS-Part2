7f4d3da89c2a492cdc8ed9ac7ca7ca62796461d9
ViolettaFilimonova <violetta99.filimonova@mail.ru>
2022-02-18 16:33:34 +0300

    create class Busket

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// class Busket extends ProductList{ //хочу чтобы значения карточков товара (конструктор и массив товаров) отображалось в карзине
+//     constructor( //количество товара  ) {
+//
+// //    }
+//
+//     outputElements(){
+//         //выводить элементы в корзину HTML
+//     }
+//
+// }
+
+
+
+
+
 class ProductList{
 
     constructor(container = '.products') {
@@ -50,28 +65,6 @@ class ProductItem{
 }
 
 new ProductList()
-// const products = [
-//     {id: 1, title: 'Notebook', price: 1000},
-//     {id: 2, title: 'Mouse', price: 100},
-//     {id: 3, title: 'Keyboard', price: 250},
-//     {id: 4, title: 'Gamepad', price: 150},
-// ]
-//
-// const renderProduct = (title, price) => {
-//     return  `<div class="product-item">
-//                  <h3>${title}</h3>
-//                  <p>${price}</p>
-//                  <button class="by-btn">Добавить</button>
-//             </div>`
-// }
-//
-// const renderProducts = list =>{
-//     document.querySelector('.products').insertAdjacentHTML(
-//         'beforeend',
-//         list.map(item => renderProduct(item.title, item.price)).join('')
-//     )
-// }
-//
-// renderProducts(products)
-//
-//
+
+
+
