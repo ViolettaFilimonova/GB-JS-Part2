bde3274de57e02d4f861c7bc0f930e4c15bb372a
ViolettaFilimonova <violetta99.filimonova@mail.ru>
2022-03-03 15:52:27 +0300

    add  vue elements

--- main.js
+++ main.js
@@ -17,70 +17,70 @@
 
 
 
+//
+// class ProductList{
+//
+//     constructor(container = '.products') {
+//         this._container = document.querySelector(container)
+//         this._goods = []
+//         this._allProducts = []
+//         this.sum = 0
+//         this.boxSum = document.querySelector('.all-sum')
+//
+//         this._fetchGoods()
+//         this._render()
+//         this.getSumAllProducts()
+//     }
 
-class ProductList{
-
-    constructor(container = '.products') {
-        this._container = document.querySelector(container)
-        this._goods = []
-        this._allProducts = []
-        this.sum = 0
-        this.boxSum = document.querySelector('.all-sum')
-
-        this._fetchGoods()
-        this._render()
-        this.getSumAllProducts()
-    }
 
 
+    // _fetchGoods(){
+    //     this._goods = [
+    //         {id: 1, title: 'Notebook', price: 1000},
+    //         {id: 2, title: 'Mouse', price: 100},
+    //         {id: 3, title: 'Keyboard', price: 250},
+    //         {id: 4, title: 'Gamepad', price: 150},
+    //     ]
+    // }
 
-    _fetchGoods(){
-        this._goods = [
-            {id: 1, title: 'Notebook', price: 1000},
-            {id: 2, title: 'Mouse', price: 100},
-            {id: 3, title: 'Keyboard', price: 250},
-            {id: 4, title: 'Gamepad', price: 150},
-        ]
-    }
-
-    _render(){
-        for (const product of this._goods){
-             const productObject = new ProductItem(product)
-            this._allProducts.push(productObject)
-            this._container.insertAdjacentHTML('beforeend', productObject.getHYMLString())
-        }
-    }
+    // _render(){
+    //     for (const product of this._goods){
+    //          const productObject = new ProductItem(product)
+    //         this._allProducts.push(productObject)
+    //         this._container.insertAdjacentHTML('beforeend', productObject.getHYMLString())
+    //     }
+    // }
 
     //2 задание
-    getSumAllProducts(){
-        for (const product of this._goods){
-            this.sum += product.price
-        }
-        this.boxSum.insertAdjacentHTML('beforeend','Сумма всех товаров равна: ' + this.sum)
-    }
-}
+//     getSumAllProducts(){
+//         for (const product of this._goods){
+//             this.sum += product.price
+//         }
+//         this.boxSum.insertAdjacentHTML('beforeend','Сумма всех товаров равна: ' + this.sum)
+//     }
+// }
 
 
 
-class ProductItem{
-    constructor(product, img = 'https://via.placeholder.com/200X150') {
-        this.title = product.title
-        this.price = product.price
-        this.img = img
-        this.id = product.id
-    }
-    getHYMLString(){
-    return  `<div class="product-item" data-id = "${this.id}">
-                 <img src="${this.img}" alt="Some img">
-                 <div class="desc">
-                     <h3>${this.title}</h3>
-                     <p>${this.price} \u20bd</p>
-                     <button class="buy-btn">Добавить</button>
-                </div>
-            </div>`
-    }
-}
+// class ProductItem{
+//     constructor(product, img = 'https://via.placeholder.com/200X150') {
+//         this.title = product.title
+//         this.price = product.price
+//         this.img = img
+//         this.id = product.id
+//     }
+    // getHYMLString(){
+    // return  `<div class="product-item" data-id = "${this.id}">
+    //              <img src="${this.img}" alt="Some img">
+    //              <div class="desc">
+    //                  <h3>${this.title}</h3>
+    //                  <p>${this.price} \u20bd</p>
+    //                  <button class="buy-btn">Добавить</button>
+    //             </div>
+    //         </div>`
+    // }
+//}
 
-new ProductList()
+// new ProductList()
 
 
